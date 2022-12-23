26888617915b1c6b2dd6fbf18cb7880a413a04f7
Nil Késede <nilkesede@gmail.com>
2022-12-23 10:45:32 -0300

    chore: update s3 library
    using s3 client v3

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,20 +1,24 @@
 import { promisify } from 'util'
-import AWS from 'aws-sdk'
+import { S3Client } from '@aws-sdk/client-s3'
 import multer from 'multer'
 import multerS3 from 'multer-s3'
 import { v4 as uuid } from 'uuid'
 
-AWS.config.update({
-  accessKeyId: process.env.ACCESS_KEY,
-  secretAccessKey: process.env.SECRET_KEY,
-  region: process.env.REGION
+const { ACCESS_KEY, SECRET_KEY, REGION, BUCKET_NAME } = process.env
+
+const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: `${ACCESS_KEY}`,
+    secretAccessKey: `${SECRET_KEY}`
+  },
+  region: REGION
 })
 
 const upload = promisify(
   multer({
     storage: multerS3({
-      s3: new AWS.S3(),
-      bucket: process.env.BUCKET_NAME,
+      s3: s3Client,
+      bucket: `${BUCKET_NAME}`,
       acl: 'public-read',
       contentType: multerS3.AUTO_CONTENT_TYPE,
       key: (_, __, cb) => cb(null, uuid())
