3cbc1b5755f246a2e0a375cbff583763d444573b
Nil Késede <nilkesede@gmail.com>
2023-12-09 17:16:10 -0300

    feat: add loading bar

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,3 +1,35 @@
-export default function Loading() {
-  return <p>Loading</p>
+import styled, { keyframes } from 'styled-components'
+
+const greenglow = keyframes`
+    from {
+        left:-50%;
+    }
+    to {
+        left:100%;
+    }
+`
+
+const ProgressBar = styled.div`
+  width: 100%;
+  height: 6px;
+  border-radius: 8px;
+  position: relative;
+  overflow: hidden;
+  background: ${({ theme }) => theme.colors.backgroundMuted};
+  margin: 5px;
+  &:after {
+    content: ' ';
+    display: block;
+    width: 50%;
+    height: 6px;
+    border-radius: 8px;
+    position: absolute;
+    animation: ${greenglow} 1s linear infinite;
+    z-index: 2;
+    background: ${({ theme }) => theme.colors.primary};
+  }
+`
+
+export default function Loading({ label }: { label: string }) {
+  return <ProgressBar aria-label={label} />
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,7 +5,7 @@ import { DefaultTheme } from 'styled-components'
 export const lightTheme: DefaultTheme = {
   colors: {
     background: '#FFFFFF',
-    backgroundMuted: '#F6F8FB',
+    backgroundMuted: '#F2F2F2',
     primary: '#2F80ED',
     inverse: '#FFFFFF',
     text: '#4F4F4F',
